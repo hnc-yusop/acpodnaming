aa4254f2ea37367514d4cbca27fb4e75077c14b9
Oren Oichman <ooichman@redhat.com>
2020-08-04 13:30:47 +0300

    adding src pkg bin

---
+++ src/acpodnaming/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	  "fmt"
+	  "encoding/json"
+	  "io/ioutil"
+	  "net/http"
+	  "github.com/golang/glog"
+
+	  // for Kubernetes 
+	  "k8s.io/api/admission/v1beta1"
+	  "k8s.io/api/core/v1"
+	  matav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	  "regexp"
+)
+
+type myValidServerhandler struct {
+
+}
+
+func (gs *myValidServerhandler) serve(w http.ResponseWriter, r *http.Request) {
+	var Body []byte
+	if r.Body != nil {
+		if data , err := ioutil.ReadAll(r.Body); err == nil {
+			Body = data
+		}
+	}
+
+	if len(Body) == 0 {
+		glog.Error("Unable to retrive Body from API")
+		http.Error(w,"Empty Body", http.StatusBadRequest)
+		return
+	}
+
+	glog.Info("Received Request")
+
+	if r.URL.Path != "/validate" {
+		glog.Error("Not a Validataion String")
+		http.Error(w,"Not a Validataion String", http.StatusBadRequest)
+		return
+	}
+
+	arRequest := v1beta1.AdmissionReview{}
+	if err := json.Unmarshal(Body, arRequest); err != nil {
+		glog.Error("incorrect Body")
+		http.Error(w, "incorrect Body", http.StatusBadRequest)
+		return
+	}
+
+	raw := arRequest.Request.Object.Raw
+	pod := v1.Pod{}
+	if err := json.Unmarshal(raw, &pod); err != nil {
+		glog.Error("Error Deserializing Pod")
+		return
+	}
+
+	podnamingReg := regexp.MustCompile(`kuku`)
+	if podnamingReg.MatchString(string(pod.Name)) {
+		return
+	} else {
+		glog.Error("the pod does not contain \"kuku\"")
+		http.Error(w, "the pod does not contain \"kuku\"", http.StatusBadRequest)
+		return
+	}
+
+	arResponse := v1beta1.AdmissionReview {
+		Response: &v1beta1.AdmissionResponse {
+			Allowed: false,
+			Result: &matav1.Status{
+				Message: "we are just making sure the Pod has the right Name",
+			},
+		},
+	}
+
+	resp, err := json.Marshal(arResponse)
+	if err != nil {
+		glog.Error("Can't encode response:", err)
+		http.Error(w, fmt.Sprintf("couldn't encode response: %v", err), http.StatusInternalServerError)
+	}
+
+	glog.Infof("Ready to write  response ...")
+	if _, err := w.Write(resp); err != nil {
+		glog.Error("Can't write response", err)
+		http.Error(w, fmt.Sprintf("cloud not write response: %v", err), http.StatusInternalServerError)
+	}
+}
---
+++ src/acpodnaming/pod-check.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+
+	"fmt"
+	"context"
+	"crypto/tls"
+	"flag"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/golang/glog"
+)
+
+func getEnv(key, fallback string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		value = fallback
+	}
+	return value
+}
+
+var (
+	tlscert, tlskey string
+)
+
+func main() {
+
+	certpem := getEnv("CERT_FILE", "/etc/certs/cert.pem") 
+	keypem := getEnv("KEY_FILE", "/etc/certs/key.pem")
+
+	flag.StringVar(&tlscert, "tlsCertFile", certpem, "File contaains the X509 Certificate for HTTPS")
+	flag.StringVar(&tlskey, "tlsKeyFile", keypem, "File containing the X509 private key")
+
+	flag.Parse()
+
+	certs, err := tls.LoadX509KeyPair(tlscert, tlskey)
+	if err != nil {
+		glog.Errorf("Failed to load key pair: %v", err)
+	}
+
+	port := getEnv("PORT", "8080")
+
+	server := &http.Server {
+		Addr: fmt.Sprintf(":%v", port),
+		TLSConfig: &tls.Config{Certificates: []tls.Certificate{certs}},
+	}
+
+	gs := myValidServerhandler{}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/validate", gs.serve)
+	server.Handler = mux
+
+
+	go func() {
+		if err := server.ListenAndServeTLS("",""); err != nil {
+			glog.Errorf("Failed to listen and serve WEB hook Server: %v", err)
+		}
+	}()
+
+	glog.Infof("Server is running on port : %s", port)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+
+	glog.Info("Get shutdown signal, sutting down webhook server gracefully...")
+	server.Shutdown(context.Background())
+
+}
